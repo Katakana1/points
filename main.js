1722679baffe2b34c375ee28992dbe8ea48590a6
Katakana1 <40336934+Katakana1@users.noreply.github.com>
2019-07-13 08:29:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,18 @@ function G1() {
     game.G1.cost*=1.1;
     game.G1.amt++;
   }
-}
+};
 setInterval(function() {
   game.points+=(G1.amt*G1.pow)/30;
+  updt();
 }, 1/30);
+function updt(){
+  document.getElementById("points").textContent = "You have " + Math.round(game.points) + " points.";
+  if(Math.round(game.clickPower) != 1){
+    document.getElementById("ppc").textContent = "You get " + Math.round(game.clickPower) + " points every click.";
+  } else {
+    document.getElementById("ppc").textContent = "You get 1 point every click.";
+  }
+  document.getElementById("g1amt").textContent = "You have " + Math.round(game.G1.amt) + " G1s, each producing " + Math.round(game.G1.pow) + " points/sec.";
+  document.getElementById("g1cost").textContent = "Buying one costs " + Math.round(game.G1.cost) + " points.";
+};
