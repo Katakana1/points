d9d5a71f1d508c69da91a131416f0e279265732a
Katakana1 <40336934+Katakana1@users.noreply.github.com>
2019-07-13 07:26:25 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var game = {
   points: 0,
   clickPower: 1,
 }
-function click() {
+function clicky() {
   game.points+=game.clickPower;
   document.getElementById("points").textContent = "You have " + game.points + " points.";
 };
